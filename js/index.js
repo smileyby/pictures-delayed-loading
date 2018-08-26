bd9f73c8acf27e9dcbf1cb4cd2841f3e793a8efd
smileyby <1500583852@qq.com>
2018-08-26 18:46:30 +0800

    Debugging code

--- js/index.js
+++ js/index.js
@@ -1,28 +1,31 @@
-var newsRender = (function () {
-    //=> 下划线开头表示公用变量
-    var _newsData = null,
+/**
+ * @author: smileyby
+ * @description:
+ * @Date: 2018/8/26 13:17
+ */
+
+var newsRender = function () {
+    var _newsDate = null,
         _newsBox = document.getElementById('newsBox');
 
-    //=> queryData: 使用ajax获取需要绑定的数据
+    //=> queryData: 使用ajax获取到需要绑定的数据
     function queryData() {
         var xhr = new XMLHttpRequest();
         xhr.open('get', 'json/news.json', false);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200){
-                _newsData = utils.toJson(xhr.responseText);
+                _newsDate = utils.toJSON(xhr.responseText);
             }
         };
         xhr.send(null);
     }
 
-    //=> 根据获取的数据把html绑定再页面中
-    function bindHTML() {
-        if(!_newsData){
-            return;
-        }
+    //=> bindHTML：根据获取的数据把HTML绑定在页面中
+    function bindHTML(){
+        if (!_newsDate){return;}
         var str = ``;
-        for (var i = 0; i < _newsData.length; i += 1) {
-            var item = _newsData[i];
+        for (var i = 0; i < _newsDate.length; i += 1) {
+            var item = _newsDate[i];
             str += `<li>
                 <a href="${item.link}">
                     <div class="imgBox"><img src="" data-img="${item.figure}" alt=""></div>
@@ -36,36 +39,34 @@ var newsRender = (function () {
         _newsBox.innerHTML = str;
     }
 
-    //=> computed：计算出那些图片需要延迟加载，我们让其延迟加载
-    function computed(){
+    //=> computed: 计算图片延迟加载
+    function computed() {
         var imgList = _newsBox.getElementsByTagName('img');
         for (var i = 0; i < imgList.length; i += 1) {
             var curImg = imgList[i],
                 curBox = curImg.parentNode;
-            if (curImg.isLoad){
-                //=> 如果当前图片已经处理过了，我们不需要重复处理，直接进行下一次循环验证下一张图片是否需要加载即可
-                continue;
-            };
-
-            //=> 获取“图片所在盒子地边框距离body的距离A”和“浏览器地边框距离body的距离B”
+            //=> 获取“图片所在盒子底部距离body的距离A”和“浏览器底边距离body距离B”
             var A = utils.offset(curBox)['top'] + curBox.offsetHeight;
             var B = utils.winBox('clientHeight') + utils.winBox('scrollTop');
-
+            if (curImg.isLoad){
+                //=> 如果当前图片已经处理过，我们不需要重复处理，直接进入下一轮循环
+                continue;
+            }
             if (A <= B){
-                //=> 当前这张图片符合延迟加载的条件，我们开始加载
+                //=> 当前图片符合延迟加载的条件，我们开始加载
                 lazyImg(curImg);
             }
 
         }
     }
 
-    //=> lazyImg: 给某一张图片延迟加载
+    //=> lazyImg: 给某一张图片进行延迟加载
     function lazyImg(curImg){
         //=> 避免重复处理
         curImg.isLoad = true;
         var tempImg = new Image;
-        tempImg.onload = function(){
-            curImg.src = this.src;
+        tempImg.onload = function () {
+            curImg.src = tempImg.src;
             curImg.style.display = 'block';
             tempImg = null;
         };
@@ -73,20 +74,15 @@ var newsRender = (function () {
     }
 
     return {
-        init: function(){
-            //=> 模块的入口：在入口文件中协调控制先做什么再做什么
+        init: function () {
+            //=> 模块的入口：在入口中斜体哦控制先做什么在做什么
             queryData();
             bindHTML();
 
-            //=> 当页面数据都加载完成，过500ms再执行图片的延迟加载 <=> window.onload 也可以
-            setTimeout(function(){
-                computed
-            }, 500);
-
-            //=> 当滚动到具体的区域的额时候，我们把当前符合条件的图片延迟加载
-            window.scroll = computed();
+            //=> 等待页面中内容渲染完成再获取计算是否需要延迟加载
+            setTimeout(computed,  500);
+            window.onscroll = computed;
         }
     }
-})();
-
+}();
 newsRender.init();
--- js/utils.js
+++ js/utils.js
@@ -1,36 +1,32 @@
-/**
- * @author: smileyby
- * @description:
- * @Date: 2018/8/21 19:07
- */
+var utils = (function () {
+   var toJSON = function toJSON(jsonStr) {
+        if (!jsonStr){return;}
+        if ('JSON' in window){
+            return JSON.parse(jsonStr);
+        } else {
+            return eval('('+ jsonStr +')');
+        }
+    };
 
-var utils = function(){
-    var toJson = function toJSON(str){
-        return "JSON" in window ? JSON.parse(str) : eval('('+ str +')');
-    }
-
-    //=> 获取当前元素距离body的偏移量，包括左偏移和上偏移
-    var offset = function(curEle){
+    //=> offset: Gets the offset of the current element from body, including the left offset and the top offset.
+    var offset = function offset(curEle){
         var l = curEle.offsetLeft,
             t = curEle.offsetTop,
             p = curEle.offsetParent;
         while(p.tagName !== 'BODY'){
-            //=> 兼容IE8
             if(!/MSIE 8/i.test(navigator.userAgent)){
                 l += p.clientLeft;
                 t += p.clientTop;
             }
-
             l += p.offsetLeft;
             t += p.offsetTop;
-            p = curEle.offsetParent;
+            p = p.offsetParent;
         }
-
-        return {left:l, top: t}
+        return {top: t, left: l}
     };
 
-    var winBox = function(attr, value){
-        if(typeof value !== 'undefined'){
+    var winBox = function windBox(attr, value) {
+        if (typeof value !== 'undefined'){
             document.documentElement[attr] = value;
             document.body[attr] = value;
             return;
@@ -39,8 +35,8 @@ var utils = function(){
     };
 
     return {
+        toJSON: toJSON,
         offset: offset,
-        winBox: winBox,
-        toJson: toJson
+        winBox: winBox
     }
-}();
+})();
